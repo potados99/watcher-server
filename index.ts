647476f365b37088f988b2413d94ef02907d5fea
potados99 <potados99@gmail.com>
2021-01-22 14:58:14 +0900

    Connection info!

--- index.ts
+++ index.ts
@@ -5,29 +5,52 @@ import { Server } from 'socket.io';
 const app = express();
 const httpServer = new http.Server(app);
 const io = new Server(httpServer, {
-    allowEIO3: false
+    pingTimeout: 2000,
+    pingInterval: 5000
 });
+
+const nodes = io.of('/node');
+const apps = io.of('/app');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-   console.log('New socket connection!');
+nodes.use((socket, next) => {
+    // @ts-ignore
+    const {nodeId, nodeName} = socket.handshake.query;
 
-   socket.on('usb-connection', (msg: any) => {
-       console.log(`USB connected: ${msg}`);
-   });
-
-   socket.on('chat message', (msg: any) => {
-       console.log(`New chat: ${msg}`);
-       io.emit('chat message', msg);
-   });
-
-   socket.on('disconnect', () => {
-      console.log('Disconnected!');
-   });
+    if (!nodeId || !nodeName) {
+        next(new Error('No id or name!'));
+    } else {
+        next();
+        // @ts-ignore
+        socket.nodeInfo = {
+            id: nodeId,
+            name: nodeName
+        };
+    }
 });
+
+nodes.on('connection', (socket) => {
+    console.log(`Node attached: ${JSON.stringify(socket.nodeInfo)}`);
+
+    socket.on('usb:connected', (msg: any) => {
+       console.log(`USB connected: ${msg}`);
+    });
+
+    socket.on('battery', (msg: any) => {
+        console.log(`Battery: ${JSON.stringify(msg)}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Node detached.');
+    });
+});
+
+io.on('error', (err) => {
+    console.log(`Error: ${err}`);
+})
 
 httpServer.listen(8080, () => {
    console.log('Listening on 8080!');
